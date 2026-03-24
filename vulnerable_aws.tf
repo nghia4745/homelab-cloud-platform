9971014a34b4c5a13597002cc1d523c6b8a4eb4a
Nghia Tran <nghia4745@gmail.com>
2026-03-24 13:47:16 -0600

    Formatted and uncommented aws resource snippet

--- vulnerable_aws.tf
+++ vulnerable_aws.tf
@@ -1,6 +1,6 @@
 # This file is intentionally insecure for testing purposes, to be removed later
-# resource "aws_s3_bucket" "test_bucket" {
-#   bucket = "my-insecure-data-bucket-2026"
-#   # MISTAKE 1: No encryption defined
-#   # MISTAKE 2: No public access block
-# }
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "my-insecure-data-bucket-2026"
+  # MISTAKE 1: No encryption defined
+  # MISTAKE 2: No public access block
+}
