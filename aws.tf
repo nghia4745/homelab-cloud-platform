912ff819ab86b51a5e2d46feaf32cfffafa44e00
Nghia Tran <nghia4745@gmail.com>
2026-03-26 13:31:23 -0600

    Add EC2

--- aws.tf
+++ aws.tf
@@ -6,6 +6,28 @@
 # WARNING: Do NOT use these configurations in production!
 # --------------------------------------------------------
 
+# -------------------------------------------------------
+# EC2 Instance - Learning Example
+# -------------------------------------------------------
+# This will trigger Infracost to show a monthly cost estimate.
+# -------------------------------------------------------
+resource "aws_instance" "web_server" {
+  ami           = "ami-0c101f26f147fa7fd" # Standard Amazon Linux 2023 (us-east-1)
+  instance_type = "t3.micro"
+
+  # Adding the Owner tag to satisfy your CKV_CUSTOM_1 policy!
+  tags = {
+    Name  = "Nghia-Web-Server"
+    Owner = "Nghia"
+  }
+
+  # Root block device (Storage) also has a cost!
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp3"
+  }
+}
+
 ## TODO: uncomment when ready to use AWS!
 
 # S3 Bucket - intentionally misconfigured for learning
