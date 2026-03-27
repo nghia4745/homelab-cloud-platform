9fde8c7ad388d2e61bf7edbd4b4159e26c838b74
Nghia Tran <nghia4745@gmail.com>
2026-03-27 09:03:59 -0600

    Added locking for s3 bucket

--- backend.tf
+++ backend.tf
@@ -14,6 +14,7 @@ terraform {
   backend "s3" {
     bucket  = "nghia-homelab-tfstate-2026"
     key     = "dev/homelab.tfstate"
+    dynamodb_table = "nghia-homelab-tfstate-lock"
     region  = "us-east-1"
     encrypt = true
   }
